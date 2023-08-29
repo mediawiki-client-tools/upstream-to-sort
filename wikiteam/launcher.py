ef160f24474559d32ef6d89c3e3dee913e0489a1
Elsie Hupp <9206310+elsiehupp@users.noreply.github.com>
2023-08-29 08:50:26 -0400

    Add unused wikiteam scripts
    Signed-off-by: Elsie Hupp <9206310+elsiehupp@users.noreply.github.com>

---
+++ wikiteam/launcher.py
@@ -0,0 +1,237 @@
+#!/usr/bin/env python3
+
+# Copyright (C) 2011-2016 WikiTeam
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# Instructions: https://github.com/WikiTeam/wikiteam/wiki/Tutorial#Download_a_list_of_wikis
+
+import argparse
+import os
+import re
+import shutil
+import subprocess
+import sys
+import time
+from pathlib import Path
+
+from wikiteam3.dumpgenerator.config import Config
+from wikiteam3.utils import domain2prefix
+
+
+def main():
+    parser = argparse.ArgumentParser(prog="launcher")
+
+    parser.add_argument("wikispath")
+    parser.add_argument("--7z-path", dest="path7z", metavar="path-to-7z")
+    parser.add_argument("--generator-arg", "-g", dest="generator_args", action="append")
+
+    args = parser.parse_args()
+
+    wikispath = args.wikispath
+
+    # None -> literal '7z', which will find the executable in PATH when running subprocesses
+    # otherwise -> resolve as path relative to current dir, then make absolute because we will change working dir later
+    path7z = str(Path(".", args.path7z).absolute()) if args.path7z is not None else "7z"
+
+    generator_args = args.generator_args if args.generator_args is not None else []
+
+    print("Reading list of APIs from", wikispath)
+
+    wikis = None
+
+    with open(wikispath) as f:
+        wikis = f.read().splitlines()
+
+    print("%d APIs found" % (len(wikis)))
+
+    for wiki in wikis:
+        print("#" * 73)
+        print("# Downloading", wiki)
+        print("#" * 73)
+        wiki = wiki.lower()
+        # Make the prefix in standard way; api and index must be defined, not important which is which
+        prefix = domain2prefix(config=Config(api=wiki, index=wiki))
+
+        # check if compressed, in that case dump was finished previously
+        zipfilename = None
+        for f in os.listdir("."):
+            if f.endswith(".7z") and f.split("-")[0] == prefix:
+                zipfilename = f
+                break  # stop searching, dot not explore subdirectories
+
+        if zipfilename:
+            print(
+                "Skipping... This wiki was downloaded and compressed before in",
+                zipfilename,
+            )
+            # Get the archive's file list.
+            if (sys.version_info[0] == 3) and (sys.version_info[1] > 0):
+                archivecontent = subprocess.check_output(
+                    [path7z, "l", zipfilename, "-scsUTF-8"],
+                    text=True,
+                    encoding="UTF-8",
+                    errors="strict",
+                )
+                if re.search(r"%s.+-history\.xml" % (prefix), archivecontent) is None:
+                    # We should perhaps not create an archive in this case, but we continue anyway.
+                    print("ERROR: The archive contains no history!")
+                if re.search(r"SpecialVersion\.html", archivecontent) is None:
+                    print(
+                        "WARNING: The archive doesn't contain SpecialVersion.html, this may indicate that download didn't finish."
+                    )
+            else:
+                print("WARNING: Content of the archive not checked, we need 3.1+.")
+                # TODO: Find a way like grep -q below without doing a 7z l multiple times?
+            continue
+
+        # download
+        started = False  # was this wiki download started before? then resume
+        wikidir = ""
+        for f in os.listdir("."):
+            # Does not find numbered wikidumps not verify directories
+            if f.endswith("wikidump") and f.split("-")[0] == prefix:
+                wikidir = f
+                started = True
+                break  # stop searching, dot not explore subdirectories
+
+        subenv = dict(os.environ)
+        subenv["PYTHONPATH"] = os.pathsep.join(sys.path)
+
+        # time.sleep(60)
+        # Uncomment what above and add --delay=60 in the py calls below for broken wiki farms
+        # such as editthis.info, wiki-site.com, wikkii (adjust the value as needed;
+        # typically they don't provide any crawl-delay value in their robots.txt).
+        if started and wikidir:  # then resume
+            print("Resuming download, using directory", wikidir)
+            subprocess.call(
+                [
+                    sys.executable,
+                    "-m",
+                    "wikiteam3.dumpgenerator",
+                    f"--api={wiki}",
+                    "--xml",
+                    "--images",
+                    "--resume",
+                    f"--path={wikidir}",
+                ]
+                + generator_args,
+                shell=False,
+                env=subenv,
+            )
+        else:  # download from scratch
+            subprocess.call(
+                [
+                    sys.executable,
+                    "-m",
+                    "wikiteam3.dumpgenerator",
+                    f"--api={wiki}",
+                    "--xml",
+                    "--images",
+                ]
+                + generator_args,
+                shell=False,
+                env=subenv,
+            )
+            started = True
+            # save wikidir now
+            for f in os.listdir("."):
+                # Does not find numbered wikidumps not verify directories
+                if f.endswith("wikidump") and f.split("-")[0] == prefix:
+                    wikidir = f
+                    break  # stop searching, dot not explore subdirectories
+
+        prefix = wikidir.split("-wikidump")[0]
+
+        finished = False
+        if started and wikidir and prefix:
+            if subprocess.call(
+                [
+                    'tail -n 1 %s/%s-history.xml | grep -q "</mediawiki>"'
+                    % (wikidir, prefix)
+                ],
+                shell=True,
+            ):
+                print(
+                    "No </mediawiki> tag found: dump failed, needs fixing; resume didn't work. Exiting."
+                )
+            else:
+                finished = True
+        # You can also issue this on your working directory to find all incomplete dumps:
+        # tail -n 1 */*-history.xml | grep -Ev -B 1 "</page>|</mediawiki>|==|^$"
+
+        # compress
+        if finished:
+            time.sleep(1)
+            os.chdir(wikidir)
+            print("Changed directory to", os.getcwd())
+            # Basic integrity check for the xml. The script doesn't actually do anything, so you should check if it's broken. Nothing can be done anyway, but redownloading.
+            subprocess.call(
+                'grep "<title>" *.xml -c;grep "<page>" *.xml -c;grep "</page>" *.xml -c;grep "<revision>" *.xml -c;grep "</revision>" *.xml -c',
+                shell=True,
+            )
+
+            pathHistoryTmp = Path("..", prefix + "-history.xml.7z.tmp")
+            pathHistoryFinal = Path("..", prefix + "-history.xml.7z")
+            pathFullTmp = Path("..", prefix + "-wikidump.7z.tmp")
+            pathFullFinal = Path("..", prefix + "-wikidump.7z")
+
+            # Make a non-solid archive with all the text and metadata at default compression. You can also add config.txt if you don't care about your computer and user names being published or you don't use full paths so that they're not stored in it.
+            compressed = subprocess.call(
+                [
+                    path7z,
+                    "a",
+                    "-ms=off",
+                    "--",
+                    str(pathHistoryTmp),
+                    f"{prefix}-history.xml",
+                    f"{prefix}-titles.txt",
+                    "index.html",
+                    "SpecialVersion.html",
+                    "errors.log",
+                    "siteinfo.json",
+                ],
+                shell=False,
+            )
+            if compressed < 2:
+                pathHistoryTmp.rename(pathHistoryFinal)
+            else:
+                print("ERROR: Compression failed, will have to retry next time")
+                pathHistoryTmp.unlink()
+
+            # Now we add the images, if there are some, to create another archive, without recompressing everything, at the min compression rate, higher doesn't compress images much more.
+            shutil.copy(pathHistoryFinal, pathFullTmp)
+
+            subprocess.call(
+                [
+                    path7z,
+                    "a",
+                    "-ms=off",
+                    "-mx=1",
+                    "--",
+                    str(pathFullTmp),
+                    f"{prefix}-images.txt",
+                    "images/",
+                ],
+                shell=False,
+            )
+
+            pathFullTmp.rename(pathFullFinal)
+
+            os.chdir("..")
+            print("Changed directory to", os.getcwd())
+            time.sleep(1)
+
+
+if __name__ == "__main__":
+    main()
---
+++ wikiteam/uploader.py
@@ -0,0 +1,432 @@
+#!/usr/bin/env python3
+
+# Copyright (C) 2011-2016 WikiTeam
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import argparse
+import getopt
+import hashlib
+import os
+import re
+import shutil
+import subprocess
+import time
+import urllib.parse
+from io import BytesIO
+from pathlib import Path
+
+import requests
+from internetarchive import get_item
+
+from wikiteam3.dumpgenerator.config import Config
+from wikiteam3.utils import domain2prefix, getUserAgent
+
+# Nothing to change below
+convertlang = {
+    "ar": "Arabic",
+    "de": "German",
+    "en": "English",
+    "es": "Spanish",
+    "fr": "French",
+    "it": "Italian",
+    "ja": "Japanese",
+    "nl": "Dutch",
+    "pl": "Polish",
+    "pt": "Portuguese",
+    "ru": "Russian",
+}
+
+
+def log(logfile, wiki, dump, msg):
+    logfile.write(f"\n{wiki};{dump.name};{msg}")
+
+
+def read_ia_keys(config):
+    with open(config.keysfile) as f:
+        key_lines = f.readlines()
+
+        accesskey = key_lines[0].strip()
+        secretkey = key_lines[1].strip()
+
+        return {"access": accesskey, "secret": secretkey}
+
+
+# We have to use md5 because the internet archive API doesn't provide
+# sha1 for all files.
+def file_md5(path):
+    buffer = bytearray(65536)
+    view = memoryview(buffer)
+    digest = hashlib.md5()
+
+    with open(path, mode="rb") as f:
+        while True:
+            n = f.readinto(buffer)
+
+            if not n:
+                break
+
+            digest.update(view[:n])
+
+    return digest.hexdigest()
+
+
+def upload(wikis, logfile, config={}, uploadeddumps=[]):
+    ia_keys = read_ia_keys(config)
+
+    headers = {"User-Agent": getUserAgent()}
+    dumpdir = Path(config.wikidump_dir)
+
+    for wiki in wikis:
+        print("#" * 73)
+        print("# Uploading", wiki)
+        print("#" * 73)
+        wiki = wiki.lower()
+        try:
+            prefix = domain2prefix(Config(api=wiki))
+        except KeyError:
+            print("ERROR: could not produce the prefix for %s" % wiki)
+
+        wikiname = prefix.split("-")[0]
+        dumps = []
+        for f in dumpdir.iterdir():
+            if f.name.startswith("%s-" % (wikiname)) and (
+                f.name.endswith("-wikidump.7z") or f.name.endswith("-history.xml.7z")
+            ):
+                print("%s found" % f)
+                dumps.append(f)
+                # Re-introduce the break here if you only need to upload one file
+                # and the I/O is too slow
+                # break
+
+        c = 0
+        identifier = "wiki-" + wikiname
+        item = get_item(identifier)
+        first_item_exists = item.exists
+        for dump in dumps:
+            wikidate = dump.name.split("-")[1]
+            if first_item_exists and config.append_date and not config.admin:
+                identifier = "wiki-" + wikiname + "-" + wikidate
+                item = get_item(identifier)
+            if dump.name in uploadeddumps:
+                if config.prune_directories:
+                    rmpath = dumpdir / f"{wikiname}-{wikidate}-wikidump"
+                    if rmpath.exists():
+                        shutil.rmtree(rmpath)
+                        print(f"DELETED {rmpath.name}/")
+
+                if config.prune_wikidump and dump.name.endswith("wikidump.7z"):
+                    # Simplistic quick&dirty check for the presence of this file in the item
+                    print("Checking content in previously uploaded files")
+                    dumphash = file_md5(dump)
+
+                    if dumphash in map(lambda x: x["md5"], item.files):
+                        log(logfile, wiki, dump, "verified")
+                        dump.unlink()
+                        print("DELETED " + str(dump))
+                        print("%s was uploaded before, skipping..." % (dump.name))
+                        continue
+                    else:
+                        print("ERROR: The online item misses " + dump.name)
+                        log(logfile, wiki, dump, "missing")
+                        # We'll exit this if and go upload the dump
+                else:
+                    print("%s was uploaded before, skipping..." % (dump.name))
+                    continue
+            else:
+                print("%s was not uploaded before" % dump.name)
+
+            time.sleep(0.1)
+            wikidate_text = wikidate[0:4] + "-" + wikidate[4:6] + "-" + wikidate[6:8]
+            print(wiki, wikiname, wikidate, dump)
+
+            # Does the item exist already?
+            ismissingitem = not item.exists
+
+            # Logo path
+            logourl = ""
+
+            if ismissingitem or config.update:
+                # get metadata from api.php
+                # first sitename and base url
+                params = {"action": "query", "meta": "siteinfo", "format": "xml"}
+                try:
+                    r = requests.get(url=wiki, params=params, headers=headers)
+                    if r.status_code < 400:
+                        xml = r.text
+                except requests.exceptions.ConnectionError as e:
+                    pass
+
+                sitename = ""
+                baseurl = ""
+                lang = ""
+                try:
+                    sitename = re.findall(r"sitename=\"([^\"]+)\"", xml)[0]
+                except:
+                    pass
+                try:
+                    baseurl = re.findall(r"base=\"([^\"]+)\"", xml)[0]
+                except:
+                    pass
+                try:
+                    lang = re.findall(r"lang=\"([^\"]+)\"", xml)[0]
+                except:
+                    pass
+
+                if not sitename:
+                    sitename = wikiname
+                if not baseurl:
+                    baseurl = re.sub(r"(?im)/api\.php", r"", wiki)
+                # Convert protocol-relative URLs
+                baseurl = re.sub("^//", "https://", baseurl)
+                if lang:
+                    lang = (
+                        convertlang[lang.lower()]
+                        if (lang.lower() in convertlang)
+                        else lang.lower()
+                    )
+
+                # now copyright info from API
+                params = {
+                    "action": "query",
+                    "meta": "siteinfo",
+                    "siprop": "general|rightsinfo",
+                    "format": "xml",
+                }
+                xml = ""
+                try:
+                    r = requests.get(url=wiki, params=params, headers=headers)
+                    if r.status_code < 400:
+                        xml = r.text
+                except requests.exceptions.ConnectionError as e:
+                    pass
+
+                rightsinfourl = ""
+                rightsinfotext = ""
+                try:
+                    rightsinfourl = re.findall(r"rightsinfo url=\"([^\"]+)\"", xml)[0]
+                    rightsinfotext = re.findall(r"text=\"([^\"]+)\"", xml)[0]
+                except:
+                    pass
+
+                raw = ""
+                try:
+                    r = requests.get(url=baseurl, headers=headers)
+                    if r.status_code < 400:
+                        raw = r.text
+                except requests.exceptions.ConnectionError as e:
+                    pass
+
+                # or copyright info from #footer in mainpage
+                if baseurl and not rightsinfourl and not rightsinfotext:
+                    print("INFO: Getting license from the HTML")
+                    rightsinfotext = ""
+                    rightsinfourl = ""
+                    try:
+                        rightsinfourl = re.findall(
+                            r"<link rel=\"copyright\" href=\"([^\"]+)\" />", raw
+                        )[0]
+                    except:
+                        pass
+                    try:
+                        rightsinfotext = re.findall(
+                            r"<li id=\"copyright\">([^\n\r]*?)</li>", raw
+                        )[0]
+                    except:
+                        pass
+                    if rightsinfotext and not rightsinfourl:
+                        rightsinfourl = baseurl + "#footer"
+                try:
+                    logourl = re.findall(
+                        r'p-logo["\'][^>]*>\s*<a [^>]*background-image:\s*(?:url\()?([^;)"]+)',
+                        raw,
+                    )
+                    if logourl:
+                        logourl = logourl[0]
+                    else:
+                        logourl = re.findall(
+                            r'"wordmark-image">[^<]*<a[^>]*>[^<]*<img src="([^"]+)"',
+                            raw,
+                        )[0]
+                    if "http" not in logourl:
+                        # Probably a relative path, construct the absolute path
+                        logourl = urllib.parse.urljoin(wiki, logourl)
+                except:
+                    pass
+
+                # retrieve some info from the wiki
+                wikititle = "Wiki - %s" % (sitename)  # Wiki - ECGpedia
+                wikidesc = (
+                    '<a href="%s">%s</a> dumped with <a href="https://github.com/mediawiki-client-tools/mediawiki-scraper/" rel="nofollow">MediaWiki-Scraper</a> (aka WikiTeam3) tools.'
+                    % (baseurl, sitename)
+                )  # "<a href=\"http://en.ecgpedia.org/\" rel=\"nofollow\">ECGpedia,</a>: a free electrocardiography (ECG) tutorial and textbook to which anyone can contribute, designed for medical professionals such as cardiac care nurses and physicians. Dumped with <a href=\"https://github.com/WikiTeam/wikiteam\" rel=\"nofollow\">WikiTeam</a> tools."
+                wikikeys = [
+                    "wiki",
+                    "wikiteam",
+                    "wikiteam3",
+                    "mediawiki-scraper",
+                    "mediawikiScraper",
+                    "MediaWiki",
+                    sitename,
+                    wikiname,
+                ]  # ecg; ECGpedia; wiki; wikiteam; MediaWiki
+
+                if not rightsinfourl and not rightsinfotext:
+                    wikikeys.append("unknowncopyright")
+                if "www.fandom.com" in rightsinfourl and "/licensing" in rightsinfourl:
+                    # Link the default license directly instead
+                    rightsinfourl = "https://creativecommons.org/licenses/by-sa/3.0/"
+                wikilicenseurl = (
+                    rightsinfourl  # http://creativecommons.org/licenses/by-nc-sa/3.0/
+                )
+                wikirights = rightsinfotext  # e.g. http://en.ecgpedia.org/wiki/Frequently_Asked_Questions : hard to fetch automatically, could be the output of API's rightsinfo if it's not a usable licenseurl or "Unknown copyright status" if nothing is found.
+
+                wikiurl = wiki  # we use api here http://en.ecgpedia.org/api.php
+            else:
+                print("Item already exists.")
+                lang = "foo"
+                wikititle = "foo"
+                wikidesc = "foo"
+                wikikeys = "foo"
+                wikilicenseurl = "foo"
+                wikirights = "foo"
+                wikiurl = "foo"
+
+            if c == 0:
+                # Item metadata
+                md = {
+                    "mediatype": "web",
+                    "collection": config.collection,
+                    "title": wikititle,
+                    "description": wikidesc,
+                    "language": lang,
+                    "last-updated-date": wikidate_text,
+                    "subject": "; ".join(
+                        wikikeys
+                    ),  # Keywords should be separated by ; but it doesn't matter much; the alternative is to set one per field with subject[0], subject[1], ...
+                    "licenseurl": wikilicenseurl
+                    and urllib.parse.urljoin(wiki, wikilicenseurl),
+                    "rights": wikirights,
+                    "originalurl": wikiurl,
+                }
+
+            # Upload files and update metadata
+            try:
+                item.upload(
+                    str(dump),
+                    metadata=md,
+                    access_key=ia_keys["access"],
+                    secret_key=ia_keys["secret"],
+                    verbose=True,
+                    queue_derive=False,
+                )
+                retry = 20
+                while not item.exists and retry > 0:
+                    retry -= 1
+                    print(
+                        'Waitting for item "%s" to be created... (%s)'
+                        % (identifier, retry)
+                    )
+                    time.sleep(10)
+                    item = get_item(identifier)
+
+                # Update metadata
+                r = item.modify_metadata(
+                    md, access_key=ia_keys["access"], secret_key=ia_keys["secret"]
+                )
+                if r.status_code != 200:
+                    print("Error when updating metadata")
+                    print(r.status_code)
+                    print(r.text)
+
+                print(
+                    "You can find it in https://archive.org/details/%s" % (identifier)
+                )
+                uploadeddumps.append(dump.name)
+            except Exception as e:
+                print(wiki, dump, "Error when uploading?")
+                print(e)
+            try:
+                log(logfile, wiki, dump, "ok")
+                if logourl:
+                    logo = BytesIO(requests.get(logourl, timeout=10).content)
+                    if ".png" in logourl:
+                        logoextension = "png"
+                    elif logourl.split("."):
+                        logoextension = logourl.split(".")[-1]
+                    else:
+                        logoextension = "unknown"
+                    logoname = "wiki-" + wikiname + "_logo." + logoextension
+                    item.upload(
+                        {logoname: logo},
+                        access_key=ia_keys["access"],
+                        secret_key=ia_keys["secret"],
+                        verbose=True,
+                    )
+            except requests.exceptions.ConnectionError as e:
+                print(wiki, dump, "Error when uploading logo?")
+                print(e)
+
+            c += 1
+
+
+def main(params=[]):
+    parser = argparse.ArgumentParser(
+        """uploader.py
+
+This script takes the filename of a list of wikis as argument and uploads their dumps to archive.org.
+The list must be a text file with the wiki's api.php URLs, one per line.
+Dumps must be in the same directory and follow the -wikidump.7z/-history.xml.7z format
+as produced by launcher.py (explained in https://github.com/WikiTeam/wikiteam/wiki/Tutorial#Publishing_the_dump ).
+You need a file named keys.txt with access and secret keys, in two different lines
+You also need py in the same directory as this script.
+
+Use --help to print this help."""
+    )
+
+    parser.add_argument("-pd", "--prune_directories", action="store_true")
+    parser.add_argument("-pw", "--prune_wikidump", action="store_true")
+    parser.add_argument("-a", "--admin", action="store_true")
+    parser.add_argument("-c", "--collection", default="opensource")
+    parser.add_argument("-wd", "--wikidump_dir", default=".")
+    parser.add_argument("-u", "--update", action="store_true")
+    parser.add_argument("-kf", "--keysfile", default="keys.txt")
+    parser.add_argument("-lf", "--logfile", default=None)
+    parser.add_argument("-d", "--append_date", action="store_true")
+    parser.add_argument("listfile")
+    config = parser.parse_args()
+    if config.admin:
+        config.collection = "wikiteam"
+    uploadeddumps = []
+    listfile = config.listfile
+    try:
+        uploadeddumps = [
+            l.split(";")[1]
+            for l in open("uploader-%s.log" % (listfile)).read().strip().splitlines()
+            if len(l.split(";")) > 1
+        ]
+    except:
+        pass
+
+    if config.logfile is None:
+        config.logfile = "uploader-" + Path(listfile).name + ".log"
+
+    print("%d dumps uploaded previously" % (len(uploadeddumps)))
+    wikis = open(listfile).read().strip().splitlines()
+
+    with open(config.logfile, "a") as logfile:
+        upload(wikis, logfile, config, uploadeddumps)
+
+
+if __name__ == "__main__":
+    main()
